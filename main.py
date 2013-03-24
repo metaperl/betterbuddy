ea79acc0cf3e795e6d3736db915a1348f463034e
metaperl <metaperl@gmail.com>
2013-03-24 19:49:30 -0400

    fixes

--- main.py
+++ main.py
@@ -26,12 +26,6 @@ action_path = dict(
 def url_for_action(action):
     return "{0}/{1}".format(base_url,action_path[action])
 
-pay_via = dict(
-    stp='1',
-    pm='5',
-    lr='6',
-    egopay='10'
-)
 
 class Entry(object):
 
@@ -43,13 +37,17 @@ class Entry(object):
     def find_bid_button(self):
         self.bid_button = self.browser.find_by_xpath('//a[@class="bid-button-link button-small"]')
 
+    def item_price(self):
+        price = self.browser.find_by_xpath('//*[@class="bid-price"]')
+        return float(price.value[1::])
+
+
     def click_bid_button(self):
         try:
             self.bid_button.click()
         except:
             print traceback.format_exc()
-        print "\t\t\tClicked."
-        time.sleep(5)
+        print "\tClicked."
 
     def login(self):
         self.browser.visit(url_for_action('login'))
@@ -58,16 +56,36 @@ class Entry(object):
         button = self.browser.find_by_id('log_submit')
         button.click()
 
+    def report_results(self):
+        self.bids_at_finish = self.bids_left()
+        diff = self.bids_at_start - self.bids_at_finish
+        bid_cost = diff * 0.50
+        total_cost = bid_cost + self.item_price()
+        print "\tBids used: {0}. Bid Cost at .50/bid = {1}. Total cost = {2}".format(diff, bid_cost, total_cost)
+
+
     def visit_auction(self):
+        self.bids_at_start = self.bids_left()
+        print "--------------------------------------------------------"
+        print "Begin auction with {0} bids".format(self.bids_at_start)
         self.browser.visit(self.url)
+        #pdb.set_trace()
+
         self.at1 = 0
         self.find_bid_button()
         while True:
             c = self.countdown()
-            print c, "Seconds left in auction"
-            if c == 2:
+            print "{0} seconds left in auction".format(c)
+            if c is None:
+                self.report_results()
+                break
+            elif c <= 2:
                 self.click_bid_button()
                 self.find_bid_button()
+                self.report_results()
+                time.sleep(1)
+                continue
+
 
             sleep_time = self.sleep_time(c)
             print "\tSleeping for", sleep_time, "seconds"
@@ -76,11 +94,18 @@ class Entry(object):
 
     def sleep_time(self, s):
         if s <= 1:
-            return 0.01
+            return 0
         elif s == 2:
-            return 0.01
+            return 0
+        elif s == 3 or s == 4:
+            return 1
         else:
-            return s - 2
+            return s - 3
+
+    def bids_left(self):
+        div = self.browser.find_by_xpath('//*[@class="bid-balance"]')
+        #pdb.set_trace()
+        return int(div.value)
 
 
     def countdown_div_value(self):
@@ -92,12 +117,16 @@ class Entry(object):
             return self.countdown_div_value()
         elif "Ended" in v:
             print "Auction ended."
-            time.sleep(600)
+            return None
         else:
             return v
 
     def countdown(self):
-        (h,m,s) = [int(v) for v in self.countdown_div_value()]
+        c = self.countdown_div_value()
+        if c is None:
+            return None
+
+        (h,m,s) = [int(v) for v in c]
         return h*60*60 + m*60 + s
 
 
